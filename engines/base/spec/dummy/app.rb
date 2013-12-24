10f23a2392fe82e487c1a63d8331ccc0705b6015
Joakim Kolsjö <joakim.kolsjo@gmail.com>
2013-12-24 19:55:57 +0100

    Correct settings for the test env.
    Didn't get ActionController::RoutingError when a page didn't exist, just an empty response.

--- engines/base/spec/dummy/app.rb
+++ engines/base/spec/dummy/app.rb
@@ -20,6 +20,16 @@ module Dummy
     config.railties_order = [ EngineLoader.loaded_engine_classes, :main_app, :all ].flatten
     config.active_support.deprecation = :stderr
     config.secret_token = '26de0653e0461dad61dd9422cc2705de61a3b73c8ecb873b5625d0bd6a98c94b3431af8fefca80f226c3fb9fbd6849115a2b0bb8ea24b605813160fa86668be6'
+    config.cache_classes = true
+    config.serve_static_assets = true
+    config.static_cache_control = "public, max-age=3600"
+    config.whiny_nils = true
+    config.consider_all_requests_local       = true
+    config.action_controller.perform_caching = false
+    config.action_dispatch.show_exceptions = false
+    config.action_controller.allow_forgery_protection    = false
+    #config.action_mailer.delivery_method = :test
+    config.active_record.mass_assignment_sanitizer = :strict
 
     config.after_initialize do
       routes.draw do
